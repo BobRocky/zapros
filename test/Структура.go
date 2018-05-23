f9967fbaef3034f169e55cddba59ab16dca8afcc
BobRocky <kiryxa02iks@gmail.com>
2018-05-24 02:30:13 +0300

    Добавление в запрос несколько городов

--- test/Структура.go
+++ test/Структура.go
@@ -1,41 +1,91 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
 func main() {
-	type znach struct {
-		fueltank_volume         int
-		consumption             int
-		fuel_in_tank            int
-		min_fuel_balance_litres int
-		min_fuel_balance_km     int
-		min_distance            int
-		min_benefit             int
-		min_fuel_level_percent  int
-		max_distance_from_route int
-		fuel_types              int
-		nav_points              []float64
+
+	i := 1
+
+	for i <= 4 {
+		i = i + 1
+
+		type q struct {
+			x float64
+			y float64
+		}
+
+		Moscow := q{37.622504, 55.753215}
+		Peter := q{30.315868, 59.939095}
+		//Pskov := q{28.331786, 57.819365}
+		//Arkhangelsk := q{40.516939, 64.539393}
+		//Astrakhan := q{48.033574, 46.347869}
+
+		/*Krasnodar = 38.974711 45.035566
+		Sochi = 39.723062 43.585525
+		molensk = 39.723062 43.585525
+		Makhachkala = 47.504682 42.98306
+		Terrible = 45.694909 43.317776
+		Elista = 44.270181 46.308309
+		Volgograd = 44.516939 48.707103
+		Rostov = 39.414526 57.185866
+		Samara = 50.101783 53.195538
+		Saratov = 46.034158 51.533103
+		Orenburg = 55.096955 51.768199
+		Izhevsk = 53.204843 56.852593
+		Perm = 56.229398 58.010374
+		Yekaterinburg = 60,597,465 56,838,011
+		KhantyMansiysk = 69.018902 61.00318
+		Nizhnevartovsk = 76.569628 60.939716
+		Krasnoyarsk = 92.852572 56.010563
+		Chelyabinsk = 61.40259 55.160026
+		Ufa = 54.738227, 55.984619
+		Tyumen = 65.534328 57.153033
+		Omsk = 73.368212 54.989342
+		Novosibirsk = 82.92043 55.030199
+		Kemerovo = 86.087314 55.354968
+		Barnaul = 83.769948 53.355084
+		Irkutsk = 104.28066 52.286387
+		UlanUde = 107.584574 51.834464
+		Chita = 113.499432 52.033973
+		Khabarovsk = 135.071917 48.480223
+		Vladivostok = 131.885485 43.115536
+		Murmansk = 33.074981 68.970682
+		Surgut = 73.396221 61.254035
+		Urengoy = 76.680974 66.083963
+		Yakutsk = 129,732,663 62.028103*/
+
+		type znach struct {
+			fueltank_volume         int
+			consumption             int
+			fuel_in_tank            int
+			min_fuel_balance_litres int
+			min_fuel_balance_km     int
+			min_distance            int
+			min_benefit             int
+			min_fuel_level_percent  int
+			max_distance_from_route int
+			fuel_types              int
+			nav_points              []q
+		}
+
+		from := Moscow
+		to := Peter
+
+		znacheniya := znach{
+			fueltank_volume:         600,
+			consumption:             30,
+			fuel_in_tank:            100,
+			min_fuel_balance_litres: 50,
+			min_fuel_balance_km:     0,
+			min_distance:            0,
+			min_benefit:             0,
+			min_fuel_level_percent:  0,
+			max_distance_from_route: 0,
+			fuel_types:              0,
+			nav_points:              []q{from, to}}
+
+		fmt.Println(znacheniya)
 	}
-
-	znacheniya := znach{
-		fueltank_volume:         600,
-		consumption:             30,
-		fuel_in_tank:            100,
-		min_fuel_balance_litres: 50,
-		min_fuel_balance_km:     0,
-		min_distance:            0,
-		min_benefit:             0,
-		min_fuel_level_percent:  0,
-		max_distance_from_route: 0,
-		fuel_types:              0,
-		nav_points:              []float64{37.622504, 55.753215, 49.106324, 55.798551}}
-
-	fmt.Println(znacheniya)
-
-	data, _ := json.Marshal(znacheniya)
-
-	fmt.Println(data)
 }
--- zapros.go
+++ zapros.go
@@ -1,30 +1,10 @@
 package main
 
-import (
-	"io"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	tr := &http.Transport{DisableKeepAlives: true}
-	client := &http.Client{Transport: tr}
-	resp, err := client.Get("https://geocode-maps.yandex.ru/1.x/?format=json&geocode=Москва")
-	if err != nil {
-		log.Panic("Responce:", resp, "\nError:", err)
-	}
-	defer resp.Body.Close()
-	bytes := make([]byte, 1024)
-	for {
-		bytes = bytes[:cap(bytes)]
-		n, err := resp.Body.Read(bytes)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Panic(err)
-		}
-		bytes = bytes[:n]
-	}
-	log.Println(string(bytes))
+
+	Moscow := float64(37.622504) + float64(55.753215)
+	fmt.Println("sdfdsfdsfsdfs", Moscow)
+
 }
